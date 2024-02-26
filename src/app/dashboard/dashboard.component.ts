c2161d0535fa6fb7ba0dfd0204c3e43352edeb00
kamilo9809 <camilorivera09@outlook.com>
2024-02-26 03:21:15 -0500

    primer commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [RouterModule, SidemenuComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export default class DashboardComponent {
+
+}
---
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -0,0 +1,18 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-flow.component.html',
+  styleUrl: './control-flow.component.css'
+})
+export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+
+  public toggleContent (){
+    this.showContent.update(value => !value )
+  }
+
+}
