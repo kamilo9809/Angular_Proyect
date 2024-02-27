7512fb174abd7fd73d82f42eb77aa3a7c31c203a
kamilo9809 <camilorivera09@outlook.com>
2024-02-27 09:23:01 -0500

    actualizacion curso de react

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,15 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+
+type grade = 'A'|'B'|'F'
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
 export default class ControlFlowComponent {
 
   public showContent = signal(false);
+  public grade = signal<grade>('A')
+  public frameworks = signal(['Angular','Vue','Svelt','Qwik','React'])
+  public frameworks2 = signal([])
 
   public toggleContent (){
     this.showContent.update(value => !value )
